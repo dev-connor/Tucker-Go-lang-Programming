8c655920fcd6fce1f180879c93e819280b685925
Connor <kor.connor@gmail.com>
2022-09-17 18:09:17 +0900

    실수의 출력 %f 와 %g

--- hello/hello.go
+++ hello/hello.go
@@ -1,14 +1,14 @@
 package main
 
+import "fmt"
+
 var g int = 10
 
 func main() {
-	var m int = 20
+	var a int = 10
+	var b int = 20
+	var f float64 = 32799438743.8297
 
-	{
-		var s int = 50
-		println(m, s, g)
-	}
-
-	//m = s + 20 -> Error
+	fmt.Println("a:", a, "b:", b, "f:", f)
+	fmt.Printf("a: %d b: %d f: %f\n", a, b, f)
 }
