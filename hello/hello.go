e69da900b1c69b645ba997f58d85e83f87258cb2
Connor <kor.connor@gmail.com>
2022-09-17 21:36:14 +0900

    8.2.3 iota 로 간편하게 열거값 사용하기

--- hello/hello.go
+++ hello/hello.go
@@ -1,17 +1,11 @@
 package main
 
-import "fmt"
-
-func Divide(a, b int) (int, bool) {
-	if b == 0 {
-		return 0, false
-	}
-	return a / b, true
-}
+const (
+	Red int = iota
+	Blue
+	Green
+)
 
 func main() {
-	c, success := Divide(9, 3)
-	fmt.Println(c, success)
-	d, success := Divide(9, 0)
-	fmt.Println(d, success)
+	println(Red, Blue, Green)
 }
