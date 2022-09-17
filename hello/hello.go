6b8ca53f8277d1a535d467175f235f1557f0243d
Connor <kor.connor@gmail.com>
2022-09-17 22:05:50 +0900

    10.1 switch 문 동작 원리

--- hello/hello.go
+++ hello/hello.go
@@ -1,11 +1,29 @@
 package main
 
-const (
-	Red int = iota
-	Blue
-	Green
-)
+func UploadFile() (string, bool) {
+	return "file.txt", true
+}
 
 func main() {
-	println(Red, Blue, Green)
+	day := 3
+
+	switch day {
+	case 1:
+		println("첫째 날입니다.")
+		println("오늘은 팀미팅이 있습니다.")
+	case 2:
+		println("둘째 날입니다.")
+		println("오늘은 면접이 있습니다.")
+	case 3:
+		println("셋째 날입니다.")
+		println("설계안을 확정하는 날입니다.")
+	case 4:
+		println("넷째 날입니다.")
+		println("예산을 확정하는 날입니다.")
+	case 5:
+		println("다섯째 날입니다.")
+		println("최종 계약하는 날입니다.")
+	default:
+		println("프로젝트를 진행하세요.")
+	}
 }
