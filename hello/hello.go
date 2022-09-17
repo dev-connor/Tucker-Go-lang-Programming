31c2072e75623a62f81444bb9a9fe10e0ae94680
Connor <kor.connor@gmail.com>
2022-09-17 22:20:02 +0900

    10.3.3 switch 초기문

--- hello/hello.go
+++ hello/hello.go
@@ -1,29 +1,18 @@
 package main
 
-func UploadFile() (string, bool) {
-	return "file.txt", true
+func getMyAge() int {
+	return 22
 }
 
 func main() {
-	day := 3
-
-	switch day {
-	case 1:
-		println("첫째 날입니다.")
-		println("오늘은 팀미팅이 있습니다.")
-	case 2:
-		println("둘째 날입니다.")
-		println("오늘은 면접이 있습니다.")
-	case 3:
-		println("셋째 날입니다.")
-		println("설계안을 확정하는 날입니다.")
-	case 4:
-		println("넷째 날입니다.")
-		println("예산을 확정하는 날입니다.")
-	case 5:
-		println("다섯째 날입니다.")
-		println("최종 계약하는 날입니다.")
+	switch age := getMyAge(); {
+	case age < 10:
+		println("Child")
+	case age < 20:
+		println("Teenager")
+	case age < 30:
+		println("20s")
 	default:
-		println("프로젝트를 진행하세요.")
+		println("My age is", age) // age 값 사용
 	}
 }
