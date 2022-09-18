cd4cdb80b324ac06d9190d85da21c9fbc1aa971a
Connor <kor.connor@gmail.com>
2022-09-18 14:24:05 +0900

    14.2 포인터는 왜 쓰나? 구조체 값 함수로 변경하기

--- hello/hello.go
+++ hello/hello.go
@@ -2,22 +2,20 @@ package main
 
 import "fmt"
 
-type User struct {
-	A int8
-	C int8
-	E int8
-	B int
-	D int
+type Data struct {
+	value int
+	data  [200]int
+}
+
+func ChangeData(arg *Data) {
+	arg.value = 999
+	arg.data[100] = 999
 }
 
 func main() {
-	a := 10
-	b := 20
+	var data Data
 
-	p1 := &a
-	p2 := &a
-	p3 := &b
-
-	fmt.Printf("p1 == p2 : %t\n", p1 == p2)
-	fmt.Printf("p2 == p3 : %t\n", p2 == p3)
+	ChangeData(&data)
+	fmt.Printf("value = %d\n", data.value)
+	fmt.Printf("data[100] = %d\n", data.data[100])
 }
