99b66c0f8995bf2f033602d62f30573c4aa2033c
Connor <kor.connor@gmail.com>
2022-09-18 12:42:40 +0900

    학생 구조체 정의

--- hello/hello.go
+++ hello/hello.go
@@ -1,15 +1,11 @@
 package main
 
+type Student struct {
+	Name  string
+	Class int
+	No    int
+	Score float64
+}
+
 func main() {
-	b := [2][5]int{
-		{1, 2, 3, 4, 5},
-		{6, 7, 8, 9, 10},
-	}
-
-	for _, arr := range b {
-
-		for _, v := range arr {
-			print(v, " ")
-		}
-	}
 }
