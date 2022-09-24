797a769e966fb95d355efbafe7f5968bb9aba822
Connor <kor.connor@gmail.com>
2022-09-24 17:58:50 +0900

    copy 로 슬라이스 복사하기

--- hello/hello.go
+++ hello/hello.go
@@ -4,13 +4,12 @@ import "fmt"
 
 func main() {
 	slice1 := []int{1, 2, 3, 4, 5}
-	slice2 := make([]int, len(slice1))
+	slice2 := make([]int, 3, 10)
+	slice3 := make([]int, 10)
 
-	for i, v := range slice1 {
-		slice2[i] = v
-	}
+	cnt1 := copy(slice2, slice1)
+	cnt2 := copy(slice3, slice1)
 
-	slice1[1] = 100
-	fmt.Println(slice1)
-	fmt.Println(slice2)
+	fmt.Println(cnt1, slice2)
+	fmt.Println(cnt2, slice3)
 }
