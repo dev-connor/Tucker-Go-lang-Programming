71405f008083893d94bacd9c94215e9ca9996963
Connor <kor.connor@gmail.com>
2022-09-24 19:22:48 +0900

    포인터 메서드와 값 메서드

--- hello/hello.go
+++ hello/hello.go
@@ -2,15 +2,36 @@ package main
 
 import "fmt"
 
-type myInt int
+type account struct {
+	balance   int
+	firstName string
+	lastName  string
+}
 
-func (a myInt) add(b int) int {
-	return int(a) + b
+func (a1 *account) withdrawPointer(amount int) {
+	a1.balance -= amount
+}
+
+func (a2 account) withdrawValue(amount int) {
+	a2.balance -= amount
+}
+
+func (a3 account) withdrawReturnValue(amount int) account {
+	a3.balance -= amount
+	return a3
 }
 
 func main() {
-	var a myInt = 10
-	fmt.Println(a.add(30))
-	var b = 20
-	println(myInt(b).add(50))
+	var mainA *account = &account{100, "Joe", "Park"}
+	mainA.withdrawPointer(30)
+	fmt.Println(mainA.balance)
+
+	mainA.withdrawValue(20)
+	fmt.Println(mainA.balance)
+
+	var mainB account = mainA.withdrawReturnValue(20)
+	fmt.Println(mainB.balance)
+
+	mainB.withdrawPointer(30)
+	fmt.Println(mainB.balance)
 }
