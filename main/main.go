b3cc145763510e1b1cb160a91e19e0016bd1cb92
Connor <kor.connor@gmail.com>
2022-09-24 20:54:54 +0900

    21.4 함수 리터럴 (익명함수)

--- main/main.go
+++ main/main.go
@@ -2,28 +2,25 @@ package main
 
 import "fmt"
 
-func add(a, b int) int {
-	return a + b
-}
-
-func mul(a, b int) int {
-	return a * b
-}
-
 func getOperator(op string) func(int, int) int {
 	if op == "+" {
-		return add
+		return func(a, b int) int {
+			return a + b
+		}
+
 	} else if op == "*" {
-		return mul
+		return func(a, b int) int {
+			return a * b
+		}
 	} else {
 		return nil
 	}
 }
 
 func main() {
-	var operator func(int, int) int
-	operator = getOperator("*")
+	result := func(a, b int) int {
+		return a + b
+	}(3, 4)
 
-	var result = operator(3, 4)
 	fmt.Println(result)
 }
