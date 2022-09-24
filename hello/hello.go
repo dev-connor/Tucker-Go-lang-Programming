08259cb4c341815b3d0196d46f187c36b5731c79
Connor <kor.connor@gmail.com>
2022-09-24 14:44:59 +0900

    18.4.1 슬라이스 복제

--- hello/hello.go
+++ hello/hello.go
@@ -1,31 +1,16 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
-var stdin = bufio.NewReader(os.Stdin)
-
-func InputIntValue() (int, error) {
-	var n int
-	_, err := fmt.Scanln(&n)
-
-	if err != nil {
-		stdin.ReadString('\n')
-	}
-	return n, err
-}
+import "fmt"
 
 func main() {
-	for {
-		fmt.Printf("숫자값을 입력하세요:")
-		n, err := InputIntValue()
-		if err != nil {
-			fmt.Println("숫자만 입력하세요.")
-		} else {
-			fmt.Println("입력하신 숫자는 ", n, " 입니다.")
-		}
+	slice1 := []int{1, 2, 3, 4, 5}
+	slice2 := make([]int, len(slice1))
+
+	for i, v := range slice1 {
+		slice2[i] = v
 	}
+
+	slice1[1] = 100
+	fmt.Println(slice1)
+	fmt.Println(slice2)
 }
