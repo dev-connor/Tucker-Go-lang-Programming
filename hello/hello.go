9b1b72468899b0e44ba86940c2f09b5b3b514280
Connor <kor.connor@gmail.com>
2022-09-24 19:09:18 +0900

    int 에 메서드 만들기

--- hello/hello.go
+++ hello/hello.go
@@ -1,38 +1,16 @@
 package main
 
-import (
-	"fmt"
-	"sort"
-)
+import "fmt"
 
-type Student struct {
-	Name string
-	Age  int
-}
+type myInt int
 
-type Students []Student
-
-func (s Students) Len() int {
-	return len(s)
-}
-
-func (s Students) Less(i, j int) bool {
-	return s[i].Age < s[j].Age
-}
-
-func (s Students) Swap(i, j int) {
-	s[i], s[j] = s[j], s[i]
+func (a myInt) add(b int) int {
+	return int(a) + b
 }
 
 func main() {
-	s := []Student{
-		{"화랑", 31},
-		{"백두산", 52},
-		{"류", 42},
-		{"켄", 38},
-		{"송하나", 18},
-	}
-
-	sort.Sort(Students(s))
-	fmt.Println(s)
+	var a myInt = 10
+	fmt.Println(a.add(30))
+	var b = 20
+	println(myInt(b).add(50))
 }
