63f25290d5b07e0c4a49de13257ca3f25b2ec527
Connor <kor.connor@gmail.com>
2022-09-24 18:38:25 +0900

    슬라이스 중간에 값 추가

--- hello/hello.go
+++ hello/hello.go
@@ -3,13 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	slice1 := []int{1, 2, 3, 4, 5}
-	slice2 := make([]int, 3, 10)
-	slice3 := make([]int, 10)
+	slice := []int{1, 2, 3, 4, 5, 6}
+	idx := 2
 
-	cnt1 := copy(slice2, slice1)
-	cnt2 := copy(slice3, slice1)
+	//slice = append(slice[:idx], append([]int{100}, slice[idx:]...)...)
+	slice = append(slice, 0)
+	copy(slice[idx+1:], slice[idx:])
+	slice[idx] = 100
 
-	fmt.Println(cnt1, slice2)
-	fmt.Println(cnt2, slice3)
+	fmt.Println(slice)
 }
