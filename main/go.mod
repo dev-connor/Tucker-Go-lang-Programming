f6f48bdcccd57d7020c055a1cc57253beca6da2c
Connor <kor.connor@gmail.com>
2022-09-24 19:51:24 +0900

    20.2 인터페이스로 택배 보내기

--- main/go.mod
+++ main/go.mod
@@ -2,4 +2,7 @@ module main
 
 go 1.16
 
-require github.com/tuckersGo/musthaveGo/ch20/fedex v0.0.0-20210809125204-68bca0d80b54
+require (
+	github.com/tuckersGo/musthaveGo/ch20/fedex v0.0.0-20210809125204-68bca0d80b54
+	github.com/tuckersGo/musthaveGo/ch20/koreaPost v0.0.0-20210809125204-68bca0d80b54
+)
--- main/main.go
+++ main/main.go
@@ -2,14 +2,23 @@ package main
 
 import (
 	"github.com/tuckersGo/musthaveGo/ch20/fedex"
+	"github.com/tuckersGo/musthaveGo/ch20/koreaPost"
 )
 
-func SendBook(name string, sender *fedex.FedexSender) {
+type Sender interface {
+	Send(parcel string)
+}
+
+func SendBook(name string, sender Sender) {
 	sender.Send(name)
 }
 
 func main() {
-	sender := &fedex.FedexSender{}
-	SendBook("어린 왕자", sender)
-	SendBook("그리스인 조르바", sender)
+	koreaPostSender := &koreaPost.PostSender{}
+	SendBook("어린 왕자", koreaPostSender)
+	SendBook("그리스인 조르바", koreaPostSender)
+
+	fedexSender := &fedex.FedexSender{}
+	SendBook("어린 왕자", fedexSender)
+	SendBook("그리스인 조르바", fedexSender)
 }
