2c0efb015b8df1b0dc87cacf6ce6444d77f71c8f
Connor <kor.connor@gmail.com>
2022-09-25 13:46:07 +0900

    22.3 맵

--- main/main.go
+++ main/main.go
@@ -1,27 +1,17 @@
 package main
 
-import (
-	"container/ring"
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	r := ring.New(5)
-	n := r.Len()
+	m := make(map[string]string)
+	m["이화랑"] = "서울시 광진구"
+	m["송하나"] = "서울시 강남구"
+	m["백두산"] = "부산시 사하구"
+	m["최번개"] = "전주시 덕진구"
 
-	for i := 0; i < n; i++ {
-		r.Value = 'A' + i
-		r = r.Next()
-	}
+	m["최번개"] = "청주시 상당구"
 
-	for j := 0; j < n; j++ {
-		fmt.Printf("%c ", r.Value)
-		r = r.Next()
-	}
-	fmt.Println()
-
-	for j := 0; j < n; j++ {
-		fmt.Printf("%c ", r.Value)
-		r = r.Prev()
-	}
+	fmt.Printf("송하나의 주소는 %s 입니다.\n", m["송하나"])
+	fmt.Printf("백두산의 주소는 %s 입니다.\n", m["백두산"])
+	fmt.Printf("최번개의 주소는 %s 입니다.\n", m["최번개"])
 }
