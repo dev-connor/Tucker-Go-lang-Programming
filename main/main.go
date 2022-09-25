5cd1684e352f1ef278cf74861f3ffa112f2ec32d
Connor <kor.connor@gmail.com>
2022-09-26 01:15:05 +0900

    25.1.6 채널에서 데이터 대기

--- main/main.go
+++ main/main.go
@@ -7,20 +7,22 @@ import (
 )
 
 func square(wg *sync.WaitGroup, ch chan int) {
-	n := <-ch
-
-	time.Sleep(time.Second)
-	fmt.Printf("Square: %d\n", n*n)
-	wg.Done()
-
+	for n := range ch { // 2. 데이터를 계속 기다림
+		fmt.Printf("Square: %d\n", n*n)
+		time.Sleep(time.Second)
+	}
+	wg.Done() // 4. 실행되지 않음
 }
 
 func main() {
-	var wg = sync.WaitGroup{}
+	var wg sync.WaitGroup
 	ch := make(chan int)
 
 	wg.Add(1)
 	go square(&wg, ch)
-	ch <- 9
-	wg.Wait()
+
+	for i := 0; i < 10; i++ {
+		ch <- i * 2 // 1. 데이터를 넣음
+	}
+	wg.Wait() // 3. 작업 완료를 기다림
 }
