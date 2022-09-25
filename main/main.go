666ec436a18cdb092cc2bb2cd8bc919f580b3389
Connor <kor.connor@gmail.com>
2022-09-25 16:05:06 +0900

    23.3.2 패닉 전파 그리고 복구

--- main/main.go
+++ main/main.go
@@ -4,14 +4,32 @@ import (
 	"fmt"
 )
 
-func divide(a, b int) {
+func f() {
+	println("f() 함수 시작")
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("panic 복구 - ", r)
+		}
+	}()
+
+	g()
+	fmt.Println("f() 함수 끝")
+
+}
+
+func g() {
+	fmt.Printf("9 / 3 = %d\n", h(9, 3))
+	fmt.Printf("9 / 0 = %d\n", h(9, 0))
+}
+
+func h(a int, b int) int {
 	if b == 0 {
-		panic("b 는 0 일 수 없습니다.")
+		panic("제수는 0 일 수 없습니다.")
 	}
-	fmt.Printf("%d / %d = %d\n", a, b, a/b)
+	return a / b
 }
 
 func main() {
-	divide(9, 3)
-	divide(9, 0)
+	f()
+	fmt.Println("프로그램이 계속 실행됨")
 }
