8a7dbde9d255b26cbc5af17eaf1c919b864dc5d5
Connor <kor.connor@gmail.com>
2022-09-26 00:49:03 +0900

    24.7 또 다른 자원 관리 기법 - 영역을 나누는 법

--- main/main.go
+++ main/main.go
@@ -6,32 +6,36 @@ import (
 	"time"
 )
 
-type Account struct {
-	Balance int
+type Job interface {
+	Do()
+}
+
+type SqareJob struct {
+	index int
+}
+
+func (j *SqareJob) Do() {
+	fmt.Printf("%d 작업 시작\n", j.index)
+	time.Sleep(1 * time.Second)
+	fmt.Printf("%d 작업 완료 - 결과: %d\n", j.index, j.index*j.index)
 }
 
 func main() {
-	var wg sync.WaitGroup
+	var jobList [10]Job
 
-	account := &Account{0}
-	wg.Add(10)
 	for i := 0; i < 10; i++ {
+		jobList[i] = &SqareJob{i}
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(10)
+
+	for i := 0; i < 10; i++ {
+		job := jobList[i]
 		go func() {
-			for {
-				DepositAndWithdraw(account)
-			}
+			job.Do()
 			wg.Done()
 		}()
 	}
 	wg.Wait()
-}
-
-func DepositAndWithdraw(account *Account) {
-	if account.Balance < 0 {
-		panic(fmt.Sprintf("Balance should not be negative value: %d",
-			account.Balance))
-	}
-	account.Balance += 1000
-	time.Sleep(time.Millisecond)
-	account.Balance -= 1000
 }
