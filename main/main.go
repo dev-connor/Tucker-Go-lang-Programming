2a280224b46c3837b7b019c5d080075b830bd8b6
Connor <kor.connor@gmail.com>
2022-09-25 16:56:47 +0900

    24.4 동시성 프로그래밍 주의점 - 잔고 입금과 인출

--- main/main.go
+++ main/main.go
@@ -6,24 +6,32 @@ import (
 	"time"
 )
 
-var wg sync.WaitGroup
-
-func SumAtoB(a, b int) {
-	sum := 0
-	for i := a; i <= b; i++ {
-		sum += i
-	}
-	fmt.Println("계산 중 입니다.")
-	time.Sleep(3 * time.Second)
-	fmt.Printf("%d 부터 %d 까지 합계는 %d 입니다.\n", a, b, sum)
-	wg.Done()
+type Account struct {
+	Balance int
 }
 
 func main() {
+	var wg sync.WaitGroup
+
+	account := &Account{0}
 	wg.Add(10)
 	for i := 0; i < 10; i++ {
-		go SumAtoB(1, 1000000000)
+		go func() {
+			for {
+				DepositAndWithdraw(account)
+			}
+			wg.Done()
+		}()
 	}
 	wg.Wait()
-	println("END")
+}
+
+func DepositAndWithdraw(account *Account) {
+	if account.Balance < 0 {
+		panic(fmt.Sprintf("Balance should not be negative value: %d",
+			account.Balance))
+	}
+	account.Balance += 1000
+	time.Sleep(time.Millisecond)
+	account.Balance -= 1000
 }
