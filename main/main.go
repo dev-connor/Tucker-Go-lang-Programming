93925bd90e78ccee7ff98d6bab53d395f621a0f1
Connor <kor.connor@gmail.com>
2022-09-26 22:06:39 +0900

    25.1.8 일정 간격으로 실행

--- main/main.go
+++ main/main.go
@@ -6,31 +6,33 @@ import (
 	"time"
 )
 
-func square(wg *sync.WaitGroup, ch chan int, quit chan bool) {
+func square(wg *sync.WaitGroup, ch chan int) {
+	tick := time.Tick(time.Second)
+	terminate := time.After(10 * time.Second)
+
 	for {
 		select {
+		case <-tick:
+			fmt.Println("Tick")
+		case <-terminate:
+			fmt.Println("Terminated!")
+			wg.Done()
+			return
 		case n := <-ch:
 			fmt.Printf("Square: %d\n", n*n)
 			time.Sleep(time.Second)
-		case <-quit:
-			wg.Done()
-			return
 		}
-
 	}
 }
 
 func main() {
 	var wg sync.WaitGroup
 	ch := make(chan int)
-	quit := make(chan bool)
-
 	wg.Add(1)
-	go square(&wg, ch, quit)
+	go square(&wg, ch)
 
 	for i := 0; i < 10; i++ {
 		ch <- i * 2
 	}
-	quit <- true
 	wg.Wait()
 }
