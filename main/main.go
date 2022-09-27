54aadb2cafeca7d6a5f62cbaf8486f987562dab4
Connor <kor.connor@gmail.com>
2022-09-27 09:05:21 +0900

    25.2.1 작업 취소가 가능한 컨텍스트

--- main/main.go
+++ main/main.go
@@ -1,38 +1,37 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"sync"
 	"time"
 )
 
-func square(wg *sync.WaitGroup, ch chan int) {
-	tick := time.Tick(time.Second)
-	terminate := time.After(10 * time.Second)
-
-	for {
-		select {
-		case t := <-tick:
-			fmt.Println("Tick", t)
-		case end := <-terminate:
-			fmt.Println("Terminated!", end)
-			wg.Done()
-			return
-		case n := <-ch:
-			fmt.Printf("Square: %d\n", n*n)
-			time.Sleep(time.Second)
-		}
-	}
-}
+var wg sync.WaitGroup
 
 func main() {
-	var wg sync.WaitGroup
-	ch := make(chan int)
 	wg.Add(1)
-	go square(&wg, ch)
+	ctx, cancel := context.WithCancel(context.Background())
+	go PrintEverySecond(ctx)
+	time.Sleep(5 * time.Second)
+	cancel()
 
-	for i := 0; i < 10; i++ {
-		ch <- i * 2
-	}
 	wg.Wait()
+
+}
+
+func PrintEverySecond(ctx context.Context) {
+	tick := time.Tick(time.Second)
+	for {
+		select {
+		case <-ctx.Done():
+			wg.Done()
+			return
+		case <-tick:
+			fmt.Println("Tick")
+
+		}
+
+	}
+
 }
