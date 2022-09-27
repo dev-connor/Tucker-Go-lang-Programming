2c5362020c8db4bcbc32c25e07be16a809baea62
Connor <kor.connor@gmail.com>
2022-09-28 00:05:45 +0900

    27.2 단일책임원칙에 위배되는 코드

--- main/main.go
+++ main/main.go
@@ -1,45 +1,13 @@
 package main
 
-import (
-	"context"
-	"fmt"
-	"sync"
-	"time"
-)
+type FinanceReport struct { // 회계 보고서
+	report string
+}
 
-var wg sync.WaitGroup
+func (r *FinanceReport) SendReport(email string) { // 보고서 전송
+
+}
 
 func main() {
-	wg.Add(1)
-
-	ctx := context.WithValue(context.Background(), "number", 9)
-	go square(ctx)
-
-	wg.Wait()
-
-}
-
-func square(ctx context.Context) {
-	if v := ctx.Value("number"); v != nil {
-		n := v.(int)
-		fmt.Printf("Square:%d", n*n)
-	}
-	wg.Done()
-
-}
-
-func PrintEverySecond(ctx context.Context) {
-	tick := time.Tick(time.Second)
-	for {
-		select {
-		case <-ctx.Done():
-			wg.Done()
-			return
-		case <-tick:
-			fmt.Println("Tick")
-
-		}
-
-	}
 
 }
