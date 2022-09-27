68715251485320b452f194664396c98ac76b760c
Connor <kor.connor@gmail.com>
2022-09-28 00:17:36 +0900

    27.2 재무 보고서 내용 출력

--- main/main.go
+++ main/main.go
@@ -1,13 +1,31 @@
 package main
 
+type Report interface {
+	Report() string
+}
+
 type FinanceReport struct { // 회계 보고서
 	report string
 }
 
-func (r *FinanceReport) SendReport(email string) { // 보고서 전송
+func (r *FinanceReport) Report() string {
+	return r.report
+}
 
+type ReportSender struct {
+	// ...
+}
+
+func (s *ReportSender) SendReport(report Report) { // 보고서 전송
+	// Report 인터페이스 객체를 인수로 받음
+	// ...
+	print(report.Report())
 }
 
 func main() {
+	f := FinanceReport{"재무 보고서 내용"}
+	r := ReportSender{}
+
+	r.SendReport(&f)
 
 }
