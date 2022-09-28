c268c32c72562841f84309ad23ea6e6a30573362
Connor <kor.connor@gmail.com>
2022-09-28 21:24:07 +0900

    28.1.1 테스트 코드 작성하기 - 대상코드 작성

--- main/main.go
+++ main/main.go
@@ -2,38 +2,10 @@ package main
 
 import "fmt"
 
-type Event interface {
-	Register(EventListener)
+func square(x int) int {
+	return 81
 }
-
-type EventListener interface {
-	OnFire()
-}
-
-type Mail struct {
-	listener EventListener
-}
-
-func (m *Mail) Register(listener EventListener) {
-	m.listener = listener
-}
-
-func (m *Mail) OnRecv() {
-	m.listener.OnFire()
-}
-
-type Alarm struct {
-}
-
-func (a *Alarm) OnFire() {
-	// 알람
-	fmt.Println("알람! 알람!")
-}
-
-var mail = &Mail{}
-var listener EventListener = &Alarm{}
 
 func main() {
-	mail.Register(listener)
-	mail.OnRecv() // 4. 알람이 울리게 됩니다.
+	fmt.Printf("9 * 9 = %d\n", square(9))
 }
