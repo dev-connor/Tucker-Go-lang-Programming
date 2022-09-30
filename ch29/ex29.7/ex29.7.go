e78f57e780e4d8eb9a875ae8db335268e44cc65a
Connor <kor.connor@gmail.com>
2022-10-01 00:09:28 +0900

    n

--- ch29/ex29.7/ex29.7.go
+++ ch29/ex29.7/ex29.7.go
@@ -10,7 +10,6 @@ func main() {
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
 		fmt.Fprint(writer, "Hello World") // 인스턴스에 핸들러 등록
 	})
-
 	err := http.ListenAndServeTLS(":3000", "localhost.crt", "localhost.key", nil)
 
 	// 1. HTTPS 서버 시작
